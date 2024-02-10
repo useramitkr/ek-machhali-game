f7a4ed7f93b5557848b86fca5c8c134022ae95c7
Amit Kumar <107535012+useramitkr@users.noreply.github.com>
2024-02-10 11:06:44 +0530

    Add files via upload

---
+++ game-file/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ek Machli</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="fish.css">
+</head>
+<body>
+    <div class="center-screen">
+
+        <div class="gameContainer">
+            <h1>Ek Machli Game</h1>
+            
+            <div class="p2p">
+                <h3>Player vs. Player</h3>
+                <button class="enter"><a href="/fish-game.php">Let's Go!</a></button>
+            </div>
+
+            <div class="p2p">
+                <h3>Me vs. You</h3>
+                <button class="enter"><a href="/fishme-game.php">Let's Go!</a></button>
+            </div>
+
+            <p class="credit"><a href="/privacy-policy">Who is the creator of this game?</a><br><a href="www.amekr.com">Privacy Policy</a></p>
+            
+        </div>
+    </div>
+
+</body>
+</html>
